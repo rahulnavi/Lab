a13a578fa8becc966781a01a78c46eda04b41844
rahulnavi <127470234+rahulnavi@users.noreply.github.com>
2023-03-16 19:22:05 +0530

    Add files via upload

---
+++ JdkCheck.java
@@ -0,0 +1,15 @@
+public class JdkCheck
+ {
+    public static void main(String[] args) 
+{
+    System.out.println("Java Version:  19.0.2");                                                         
+	System.out.println("Java Runtime Version: 19.0.2.7.44");                                               
+	System.out.println("Java Home: /opt/jdk/jdk1.8.0_71/jre");                                               
+	System.out.println("Java Vendor: Oracle Corporation");                                                 
+	System.out.println("Java Vendor URL: http://Java.oracle.com/");                                         
+	System.out.println("Java Class Path: .");
+    }
+}
+
+
+
